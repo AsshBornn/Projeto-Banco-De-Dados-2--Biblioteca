cc9f03354b643ef90118a7bca86146a2c4b6b3f1
Erick <erickgeovane2002@gmail.com>
2025-10-06 14:48:22 -0300

    Inicio as Classes DAO

--- src/main/java/org/primeiroprojetocursooo/projetobancodedados2biblioteca/DAO/CategoriaDAO.java
+++ src/main/java/org/primeiroprojetocursooo/projetobancodedados2biblioteca/DAO/CategoriaDAO.java
@@ -1,4 +1,28 @@
 package org.primeiroprojetocursooo.projetobancodedados2biblioteca.DAO;
 
+import jakarta.persistence.EntityManager;
+import org.primeiroprojetocursooo.projetobancodedados2biblioteca.entity.Categoria;
+import org.primeiroprojetocursooo.projetobancodedados2biblioteca.util.JPAUtil;
+
 public class CategoriaDAO {
+
+    public void salvar(Categoria categoria) {
+        EntityManager em = JPAUtil.getEntityManager(); //Inicia o EntityManager
+        try {
+            em.getTransaction().begin();// Para Fazer a persistencia o EM precisa fazer, como uma transação
+            em.persist(categoria);//persistencia da categoria
+            em.getTransaction().commit();//confirma a transação
+        }catch(Exception e) {//catch para capturar excessão em caso de erro
+            if(em.getTransaction().isActive()) {//verifica se deu erro na transação
+                em.getTransaction().rollback();//desfaz a transação
+            }
+            e.printStackTrace();//print do erro
+        }
+        finally {//garante que o EM seja fechado mesmo se der erro
+            em.close();
+        }
+    }
+
+
+
 }
